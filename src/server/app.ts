14c24a523a197c7b17aae6058547f51332d7c33d
alekseyka221 <alekseyka221@gmail.com>
2021-04-06 15:07:32 +0200

    настройка проекта

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,1 +1,9 @@
-console.log("app");
+import * as express from "express";
+const app = express();
+app.get("/", (_request, response) => {
+	response.send("Hello nagato");
+})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+	console.log(`Server is running in http://localhost:${PORT}`)
+})
